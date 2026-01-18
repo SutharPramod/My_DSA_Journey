a811d154bc53d3eec5e5edb3abe2e24a67cb6cdc
Pramod Suthar <pramod01010107@gmail.com>
2026-01-18 18:13:53 +0530

    Search in Rotated Array

--- Array/Search in Rotated Array/Search_in_rotated_array.cpp
+++ Array/Search in Rotated Array/Search_in_rotated_array.cpp
@@ -8,11 +8,11 @@ int search(vector<int> nums, int target){
     while(s <= e){
         int mid = s + (e - s)/2;
         if(nums[mid] == target) return mid;
-        if(nums[s] <= nums[mid]){
+        if(nums[s] <= nums[mid]){ //left
             if(nums[s] <= target && target <= nums[mid]) e = mid - 1;
             else s = mid + 1;
         }
-        else{
+        else{ //right
             if(nums[mid] <= target && target <= nums[e]) s = mid + 1;
             else e = mid - 1;
         }
