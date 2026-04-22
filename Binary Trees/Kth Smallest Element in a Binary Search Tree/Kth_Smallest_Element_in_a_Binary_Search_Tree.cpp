dfe5a1a9bac63a07dd34d50f8057ebfc03c200a1
Pramod Suthar <pramod01010107@gmail.com>
2026-04-22 11:15:34 +0530

    Kth Smallest Element in a Binary Search Tree

---
+++ Binary Trees/Kth Smallest Element in a Binary Search Tree/Kth_Smallest_Element_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int result = -1 , count = 0;
+
+    void helper(TreeNode* root, int k){
+        if(!root) return;
+
+        helper(root->left,k);
+
+        count++;
+        if(count == k) result = root->val;
+
+        helper(root->right,k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        helper(root,k);
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    
+    // Constructing Tree:
+    //      3
+    //     / \
+    //    1   4
+    //     \
+    //      2
+    TreeNode* root = new TreeNode(3, 
+                        new TreeNode(1, nullptr, new TreeNode(2)), 
+                        new TreeNode(4));
+    
+    int k = 1;
+    int result = sol.kthSmallest(root, k);
+    
+    // Output result
+    cout << k << "st Smallest Element: " << result << endl;
+
+    return 0;
+}
